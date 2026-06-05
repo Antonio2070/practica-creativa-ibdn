651a635f1b4b1cc0eb06c32b493e48aa4b9d429f
antonio <antonio@ubuntu.myguest.virtualbox.org>
2026-06-05 17:15:49 +0200

    añadiendo sbt package a las inicializaciones

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -7,6 +7,15 @@ echo "======================================"
 
 ./scripts/download_dependencies.sh
 
+if [ ! -f flight_prediction/target/scala-2.13/flight_prediction_2.13-0.1.jar ]; then
+    echo "JAR no encontrado. Compilando..."
+    cd flight_prediction
+    sbt package
+    cd ..
+else
+    echo "JAR ya compilado."
+fi
+
 docker-compose up -d --build
 
 echo ""
--- scripts/k8s_start.sh
+++ scripts/k8s_start.sh
@@ -11,6 +11,15 @@ eval $(minikube docker-env)
 
 ./scripts/download_dependencies.sh
 
+if [ ! -f flight_prediction/target/scala-2.13/flight_prediction_2.13-0.1.jar ]; then
+    echo "JAR no encontrado. Compilando..."
+    cd flight_prediction
+    sbt package
+    cd ..
+else
+    echo "JAR ya compilado."
+fi
+
 echo "Construyendo imágenes..."
 docker build -t practica-spark:latest -f Dockerfile.spark .
 docker build -t practica-web:latest -f Dockerfile.web .
